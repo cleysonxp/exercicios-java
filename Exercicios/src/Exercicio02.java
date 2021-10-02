3692820285f7ce57aaf1ca7d04d450b6debc6cba
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-10-02 13:22:30 -0300

    Programa pedido um número para ser digitado

--- Exercicios/src/Exercicio02.java
+++ Exercicios/src/Exercicio02.java
@@ -1,8 +1,22 @@
+// Pedido um numero
+
+import java.util.Scanner;
+
+//import javax.swing.JOptionPane;
 
 public class Exercicio02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Digite um número: ");
+		int n = scan.nextInt();
+//		String n = JOptionPane.showInputDialog("Digite um número");
+		
+		
+
+		System.out.println("O número informado foi " + n);
 
 	}
 
