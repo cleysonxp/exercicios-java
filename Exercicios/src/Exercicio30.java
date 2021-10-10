6bd93a0cdd9cdb13c2c6142af2e76336c0eec613
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-10-09 23:05:06 -0300

    Programa que calcula as raizes de uma equação de segundo grau.

---
+++ Exercicios/src/Exercicio30.java
@@ -0,0 +1,33 @@
+//Programa que calcula as raizes de uma equação de segundo grau.
+import java.util.Scanner;
+
+public class Exercicio30 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Informe o valor de a: ");
+		double a = scan.nextDouble();
+		if (a == 0) {
+			System.out.println("Não é equação de segundo grau.");
+		} else {
+			System.out.print("Informe o valor de b: ");
+			double b = scan.nextDouble();
+			System.out.print("Informe o valor de c: ");
+			double c = scan.nextDouble();
+
+			double delta = (b * b) - (4 * a * c);
+			if (delta < 0) {
+				System.out.println("Delta negativo.");
+			} else {
+				double x1 = ((-b) + Math.sqrt(delta)) / (2 * a);
+				double x2 = ((-b) - Math.sqrt(delta)) / (2 * a);
+
+				System.out.println("x1 = " + x1);
+				System.out.println("x2 = " + x2);
+			}
+		}
+
+	}
+
+}
