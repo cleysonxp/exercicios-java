061e040d6de85a1dbc625fb9922016e6001be95d
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-10-17 23:34:48 -0300

    Programa que exibira numero de 0 a 20 na diagonal e na vertical.

---
+++ Exercicios/src/Exercicio43.java
@@ -0,0 +1,16 @@
+//Programa que exibira numero de 0 a 20 na diagonal e na vertical.
+public class Exercicio43 {
+
+	public static void main(String[] args) {
+
+		for(int i =0 ;i<=20;i++) {
+			System.out.print(i+" - ");
+		}
+		
+		for(int i =0 ;i<=20;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
