fff191702e8b23fb7e21e7d7ed01e25fd4c17891
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-10-18 22:25:04 -0300

    Programa que imprime os números impares do zero até 50.

---
+++ Exercicios/src/Exercico46.java
@@ -0,0 +1,16 @@
+//Programa que imprime os números impares do zero até 50.
+import java.util.Iterator;
+
+public class Exercico46 {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 50; i++) {
+			if(i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+		}
+
+	}
+
+}
