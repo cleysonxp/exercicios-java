0be41b1f910949d2c9812716273d174b21c5351a
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-10-20 22:06:35 -0300

    Programa que pede dois números e faz a soma dos numeros entre eles.

---
+++ Exercicios/src/Exercicio48.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Exercicio48 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Informe o primeiro número: ");
+		int num1 = scan.nextInt();
+		System.out.print("Informe o segundo número: ");
+		int num2 = scan.nextInt();
+		int total = 0;
+
+		System.out.println("---------------------");
+
+		if (num1 < num2) {
+
+			for (int i = num1; i <= num2; i++) {
+				total += i;
+			}
+		} else if (num1 > num2) {
+			for (int i = num2; i <= num1; i++) {
+				total += i;
+			}
+		}
+
+		System.out.println("A soma dos entre " + num1 + " e " + num2 + " eh: " + total);
+
+	}
+
+}
