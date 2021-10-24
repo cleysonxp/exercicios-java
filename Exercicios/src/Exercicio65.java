2b54b27c5477e1e8be37d989ec083773e3184a49
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-10-24 12:20:44 -0300

    Programa que informa se o número digitado é primo ou não.

---
+++ Exercicios/src/Exercicio65.java
@@ -0,0 +1,25 @@
+
+//Programa que informa se o número digitado é primo ou não.
+import java.util.Scanner;
+
+public class Exercicio65 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Digite um número: ");
+		int num = scan.nextInt();
+
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				System.out.println("Numero digitado não é primo");
+				break;
+			} else {
+				System.out.println("Numero digitado é primo");
+				break;
+			}
+		}
+
+	}
+
+}
