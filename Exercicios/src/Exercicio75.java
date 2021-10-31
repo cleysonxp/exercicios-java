79395f71c8ba714ef4a9badcac8474744037711e
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-10-30 21:13:38 -0300

    Programa onde o vetor B tem os valores que o vetor A multiplicado por 2.

---
+++ Exercicios/src/Exercicio75.java
@@ -0,0 +1,32 @@
+//Programa onde o vetor B tem os valores que o vetor A multiplicado por 2.
+import java.util.Scanner;
+
+public class Exercicio75 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[8];
+		int[] vetorB = new int[vetorA.length];
+
+		for (int i = 0; i < vetorA.length; i++) {
+
+			System.out.print("Informe o valor da posição " + i + ": ");
+			vetorA[i] = scan.nextInt();
+			vetorB[i] = vetorA[i] * 2;
+		}
+		System.out.println();
+
+		System.out.print("Vetor A = ");
+		for(int i = 0; i<vetorA.length;i++ ) {
+			System.out.print(vetorA[i]+" ");
+		}
+		
+		System.out.println();
+		System.out.print("Vetor B = ");
+		for(int i = 0; i<vetorB.length;i++ ) {
+			System.out.print(vetorB[i]+" ");
+		}	
+	}
+}
