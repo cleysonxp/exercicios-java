8f5ecec7ac429549f56f8442ba89b9b26dbb05ba
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-11-28 22:56:30 -0300

    Programa que pede 20 números para o vetor A e armazena no inicicio do vetor B os números pares e depois os números impares.

---
+++ Exercicios/src/Exercicio101.java
@@ -0,0 +1,51 @@
+//Programa que pede 20 números para o vetor A e armazena no inicicio do vetor B os números pares e depois os números impares. 
+import java.util.Scanner;
+
+public class Exercicio101 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[20];
+		int[] vetorB = new int[vetorA.length]; // pares e impares
+
+		for (int i = 0; i < vetorA.length; i++) {
+
+			System.out.print("Entre com o valor da posição A - " + i + ": ");
+			vetorA[i] = scan.nextInt();
+		}
+
+		int posB = 0;
+
+		for (int i = 0; i < vetorA.length; i++) {
+
+			if (vetorA[i] % 2 == 0) {
+				vetorB[posB] = vetorA[i];
+				posB++;
+			}
+		}
+
+		for (int i = 0; i < vetorA.length; i++) {
+
+			if (vetorA[i] % 2 != 0) {
+				vetorB[posB] = vetorA[i];
+				posB++;
+			}
+		}
+
+		System.out.print("Vetor A = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i] + " ");
+		}
+		System.out.println();
+
+		System.out.print("Vetor B = ");
+		for (int i = 0; i < posB; i++) {
+			System.out.print(vetorB[i] + " ");
+		}
+		System.out.println();
+
+	}
+
+}
