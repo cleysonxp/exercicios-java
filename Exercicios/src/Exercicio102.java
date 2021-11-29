ef4fb698ea90324c91b3612aaded7941b0af5b5b
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-11-28 22:51:02 -0300

    Programa que pede 5 números diferentes e imprime a tabuada de cada.

---
+++ Exercicios/src/Exercicio102.java
@@ -0,0 +1,31 @@
+//Programa que pede 5 números diferentes e imprime a tabuada de cada.
+import java.util.Scanner;
+
+public class Exercicio102 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[5];
+
+		for (int i = 0; i < vetorA.length; i++) {
+
+			System.out.print("Entre com o valor da posição A - " + i + ": ");
+			vetorA[i] = scan.nextInt();
+		}
+
+		for (int i = 0; i < vetorA.length; i++) {
+
+			System.out.println("Tabuada de " + vetorA[i]);
+
+			for (int j = 1; j <= 10; j++) {
+
+				System.out.println(j + " * " + vetorA[i] + " = " + (vetorA[i] * j));
+			}
+
+			System.out.println();
+		}
+	}
+
+}
