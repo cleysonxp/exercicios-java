7b4a46e67e8d485c7c2289b450db2b8c196c38a3
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2021-12-06 22:37:56 -0300

    Programa que pede 10 números e informa quais são os divisores desses números.

---
+++ Exercicios/src/Exercicio105.java
@@ -0,0 +1,33 @@
+//Programa que pede 10 números e informa quais são os divisores desses números.
+import java.util.Scanner;
+
+public class Exercicio105 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int[] vetorA = new int[10];
+
+		for (int i = 0; i < vetorA.length; i++) {
+
+			System.out.print("Entre com o valor da posição A - " + i + ": ");
+			vetorA[i] = scan.nextInt();
+		}
+
+		for (int i = 0; i < vetorA.length; i++) {
+
+			System.out.println("Analizando o número " + vetorA[i]);
+
+			for (int j = 1; j < vetorA[i]; j++) {
+				if (vetorA[i] % j == 0) {
+					System.out.println(j + " é divisor");
+				}
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
